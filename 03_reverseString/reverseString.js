c46ae6c8b091a004594a5dd1ac95d414c41a8bb2
JGLucio <84593184+JGLucio@users.noreply.github.com>
2025-01-07 12:41:14 +0100

    Solve ex3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+const reverseString = function(text) {
+
+    let str = [];
+    let j = 0;
+    for(let i=text.length-1;i>=0;i--){
+        str[j] = text[i];
+        j++;
+    }
+    
+    return str.join("");
 
 };
 
