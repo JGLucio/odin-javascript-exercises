814c8d853d19e0d0d174c9876f4433a328746fad
JGLucio <84593184+JGLucio@users.noreply.github.com>
2025-01-07 13:32:04 +0100

    Solve ex05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
 
+    if(a%1 != 0 || b%1 != 0){
+        return 'ERROR';
+    }
+
+    if(a<0 || b<0){
+        return 'ERROR';
+    }
+    
+    if(typeof a != 'number' || typeof b != 'number'){
+        return 'ERROR'
+    }
+
+    if(a>b){
+        for(b; b<=a; b++){
+            sum += b;
+        }
+    }else{
+        for(a; a<=b; a++){
+            sum += a;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
