8e090b078459a737b0bb2b81935ae5157b90c90c
JGLucio <84593184+JGLucio@users.noreply.github.com>
2025-01-07 13:06:10 +0100

    Solve ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(text, ...args) {
+    
+    for(let i = 0; i<args.length; i++){
+        for(let j = 0; j<text.length; j++){
+            if(text[j] === args[i]){
+                text.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return text;
 };
 
 // Do not edit below this line
