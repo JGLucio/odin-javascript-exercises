57c89c2e8186d52ecd09e9d2c565099cb45da9a2
JGLucio <84593184+JGLucio@users.noreply.github.com>
2025-01-07 14:03:21 +0100

    Solve ex7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let celsius = 0;
+
+  celsius = (far - 32) * (5/9);
+
+  if(celsius%1 != 0){
+    celsius = celsius.toFixed(1);
+  }
+
+  return Number(celsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let far = 0;
+
+  far = celsius * (9/5) + 32;
+
+  if(far%1 != 0){
+    far = far.toFixed(1);
+  }
+
+  return Number(far);
 };
 
 // Do not edit below this line
