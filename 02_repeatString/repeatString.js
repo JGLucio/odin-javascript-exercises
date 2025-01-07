8ccf2f650f2108fef3902ac721bf476d30fe04ab
JGLucio <84593184+JGLucio@users.noreply.github.com>
2025-01-07 12:19:42 +0100

    Solve ex2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
+    if(num<0){
+        return 'ERROR';
+    }
+
+    let str = '';
+    for(let n = 0; n<num; n++){
+        str = str + text;
+    }
+    return str;
 
 };
 
