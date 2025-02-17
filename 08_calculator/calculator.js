8b6e0b8e42d052463295ed3cfb0c22283fc9e848
JGLucio <84593184+JGLucio@users.noreply.github.com>
2025-02-17 22:10:16 +0100

    Solve ex8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+  let init = 0;
+  const sumTotal = sumArray.reduce(
+    (accum, current) => accum + current,
+    init,
+  );
+
+	return sumTotal;
 };
 
-const multiply = function() {
+const multiply = function(mulArray) {
+  let init = 1;
+  const mulTotal = mulArray.reduce(
+    (accum, current) => accum * current,
+    init,
+  );
 
+	return mulTotal;
 };
 
-const power = function() {
-	
+const power = function(num, powerNum) {
+	let powerResult = 1;
+  if (powerNum === 0){
+    return powerResult;
+  }
+
+  for (let i = 0; i<powerNum; i++){
+    powerResult = powerResult * num;
+  }
+
+  return powerResult;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+
+  for(let i = num; i >0; i--){
+    result = result * i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
